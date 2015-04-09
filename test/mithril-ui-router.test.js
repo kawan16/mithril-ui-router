cfe423872688b81daf1fb708faab507d7f48a15c
Karl <karl.devooght@gmail.com>
2015-04-09 23:11:44 +0200

    Set up unit test environment

---
+++ test/mithril-ui-router.test.js
@@ -0,0 +1,30 @@
+
+/* global describe, it, expect */
+
+
+/* Route method */
+describe('mx.route()' , function() {
+
+    it( 'should be a function' , function() {
+        expect( typeof mx.route ).toBe( 'function' );
+    })
+
+});
+
+/* Go method */
+describe('mx.go()' , function() {
+
+    it( 'should be a function' , function() {
+        expect( typeof mx.go ).toBe( 'function' );
+    })
+
+});
+
+/* Param method */
+describe('mx.param()' , function() {
+
+    it( 'should be a function' , function() {
+        expect( typeof mx.param ).toBe( 'function' );
+    })
+
+});
