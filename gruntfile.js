bfe05a0b7fb744466708b33f06008f03b8891348
Karl <karl.devooght@gmail.com>
2015-04-11 00:06:11 +0200

    -

--- gruntfile.js
+++ gruntfile.js
@@ -63,7 +63,7 @@ module.exports = function (grunt) {
 
         // Testing via jasmine
         jasmine: {
-            src: [ '<%= config.lib %>/**/*.js' , '<%= config.src %>/mithril-ui-router.js'  ],
+            src: [ '<%= config.lib %>/**/*.js' ,  '<%= config.src %>/mithril-ui-router.js'  ],
             options: {
                 specs : '<%= config.test %>/**/*.js'
             }
